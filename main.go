d310ad47bcf2f15ee2a8eb4098e08f6be61341f9
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-20 18:28:37 -0700

    Extract out getSubjects.

--- main.go
+++ main.go
@@ -30,25 +30,7 @@ func main() {
 	})
 
 	router.GET("/api/v2/subjects", func(c *gin.Context) {
-		ch := make(chan Subjects)
-		maxPages := 5
-		for page := 1; page <= maxPages; page++ {
-			go getSubjects(page, ch)
-		}
-		
-		subjects := <-ch
-		if (int(subjects.Pages.Last) > maxPages) {
-			for page := maxPages+1; page <= int(subjects.Pages.Last); page++ {
-				go getSubjects(page, ch)
-			}
-			maxPages = int(subjects.Pages.Last)
-		}
-
-		for page := 2; page <= maxPages; page++ {
-			subjectsPage := <-ch
-			// fmt.Printf("%d/%d: Appending page %d to page %d\n", page, maxPages, subjectsPage.Pages.Current, subjects.Pages.Current)
-			subjects.Data = append(subjects.Data, subjectsPage.Data...)
-		}
+		subjects := getSubjects()
 
 		fmt.Printf("%d subjects pages in total\n", subjects.Pages.Last)
 		fmt.Printf("data has length %d\n", len(subjects.Data))
@@ -58,6 +40,32 @@ func main() {
 	router.Run(":" + port)
 }
 
+func getSubjects() *Subjects {
+	ch := make(chan Subjects)
+	maxPages := 5
+	for page := 1; page <= maxPages; page++ {
+		go getSubjectsPage(page, ch)
+	}
+	
+	subjects := <-ch
+	if (int(subjects.Pages.Last) > maxPages) {
+		for page := maxPages+1; page <= int(subjects.Pages.Last); page++ {
+			go getSubjectsPage(page, ch)
+		}
+		maxPages = int(subjects.Pages.Last)
+	}
+
+	for page := 2; page <= maxPages; page++ {
+		subjectsPage := <-ch
+		// fmt.Printf("%d/%d: Appending page %d to page %d\n", page, maxPages, subjectsPage.Pages.Current, subjects.Pages.Current)
+		subjects.Data = append(subjects.Data, subjectsPage.Data...)
+	}
+
+	subjects.Pages.Current = 1
+
+	return &subjects
+}
+
 func getUrl(url string) []byte {
 	start := time.Now()
 	client := &http.Client{}
@@ -65,27 +73,27 @@ func getUrl(url string) []byte {
 	req.Header.Add("Authorization", "Token token=" + os.Getenv("WANIKANI_V2_API_KEY"))
 	resp, _ := client.Do(req)
 	defer resp.Body.Close()
-	secs1 := time.Since(start).Seconds()
+	// secs1 := time.Since(start).Seconds()
 	body, _ := ioutil.ReadAll(resp.Body)
 	secs2 := time.Since(start).Seconds()
   
-	fmt.Printf("%f %f: %s\n", secs1, secs2, url)
+	fmt.Printf("%f: %s\n", secs2, url)
 
 	return body
 }
 
-func getSubjects(page int, ch chan Subjects) {
+func getSubjectsPage(page int, ch chan Subjects) {
 	body := getUrl(fmt.Sprintf("https://wanikani.com/api/v2/subjects?page=%d",page))
 	var subjects Subjects
-	start := time.Now()
+	// start := time.Now()
 	
 	err := json.Unmarshal(body, &subjects)
 	if err != nil {
 		log.Fatal("error:", err, string(body))
 	}
-	secs := time.Since(start).Seconds()
+	// secs := time.Since(start).Seconds()
 
-	fmt.Printf("%f: JSON %d\n", secs, page)
+	// fmt.Printf("%f: JSON %d\n", secs, page)
 
 	// fmt.Printf("Got subject page %d\n", page)	
 	ch <- subjects
