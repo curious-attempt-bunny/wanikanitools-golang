bb3b5db640213907978068c7afe4dac6112ac51f
Merlyn <curious.attempt.bunny@gmail.com>
2017-12-14 19:28:42 -0800

    Add database indexes.

---
+++ migrations/5_create_indexes.up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX index_scripts_api_key ON scripts (api_key);
+CREATE INDEX index_scripts_script_name ON scripts (script_name);
+CREATE INDEX index_leech_train_api_key ON leech_train (api_key);
